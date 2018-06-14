440dacea1375c930278f00096cfaf2a2938ce9d2
Tim Whiteaker <whiteaker@utexas.edu>
2018-06-14 15:27:30 -0500

    add check_inputs test

---
+++ tests/testthat/test-check_inputs.R
@@ -0,0 +1,29 @@
+context("Test the check_inputs function")
+
+test_that("all_logical checks for booleans", {
+  bool_var <- TRUE
+  not_bool_var <- 1
+  all_logical(c("bool_var", "bool_var"))
+  expect_error(all_logical(c("not_bool_var", "bool_var")))
+})
+
+test_that("all_character checks for characters", {
+  valid_var <- "happy"
+  invalid_var <- TRUE
+  all_character(c("valid_var", "valid_var"))
+  expect_error(all_character(c("invalid_var", "valid_var")))
+})
+
+test_that("all_numeric checks for numbers", {
+  valid_var <- 1
+  invalid_var <- "1"
+  all_numeric("valid_var")
+  expect_error(all_numeric(c("invalid_var", "valid_var")))
+})
+
+test_that("all_list checks for lists", {
+  valid_var <- list(1, 2, 3)
+  invalid_var <- 1
+  all_list("valid_var")
+  expect_error(all_list(c("invalid_var", "valid_var")))
+})
