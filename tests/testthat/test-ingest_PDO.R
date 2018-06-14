be2ef525afbe06992d7b1e32595a8e20ab18aa27
Tim Whiteaker <whiteaker@utexas.edu>
2018-06-14 12:09:58 -0500

    Update PDO test to point to correct folder

--- tests/testthat/test-ingest_PDO.R
+++ tests/testthat/test-ingest_PDO.R
@@ -1,7 +1,7 @@
 context("Test ingesting Pacific decadal oscillation data")
 library(ingestr)
 
-test_file <- system.file("extdata", "jisao.washington.edu_pdo_PDO.latest.txt", package = "ingestr")
+test_file <- system.file("example_data", "jisao.washington.edu_pdo_PDO.latest.txt", package = "ingestr")
 
 test_that("AUG is in list of months returned", {
   df <- ingest_PDO(test_file)
