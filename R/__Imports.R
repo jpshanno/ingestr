7929d77a89d4a0050fb5a90d153a97f170b8734a
Joe Shannon <jpshanno@mtu.edu>
2019-04-10 09:30:20 -0400

    Import & re-export pipe for use in function definitions

---
+++ R/__Imports.R
@@ -0,0 +1,8 @@
+# From https://petermeissner.de/blog/2018/04/27/re-exporting-the-pipe/
+#' re-export magrittr pipe operator
+#'
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @rdname %>%
+#' @export
+NULL
