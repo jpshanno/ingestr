ea8a3c61109aa957cacf0d671cec87d0cd790ea3
Rachael Blake <rblake@rstudio03.research.sesync.org>
2019-04-10 12:52:15 -0400

    modify directory tests

--- tests/testthat/test-ingest_directory.R
+++ tests/testthat/test-ingest_directory.R
@@ -1,9 +1,9 @@
 context("Test ingesting a directory of uniform files")
 
 test_dir <- system.file("example_data", package = "ingestr")
+df <- ingestr::ingest_directory(test_dir, pattern = ".csv")
 
 test_that("output is a dataframe", {
-  df <- ingestr::ingest_directory(test_dir)
   expect_s3_class(df, "data.frame")
 })
 
