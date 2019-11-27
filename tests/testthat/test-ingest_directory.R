b9dc76f9d025a94d1b6c29a4525c54c57a1677d0
Rachael Blake <rblake@rstudio03.research.sesync.org>
2019-11-27 10:56:25 -0500

    fixed tests of ingest_directory

--- tests/testthat/test-ingest_directory.R
+++ tests/testthat/test-ingest_directory.R
@@ -1,9 +1,16 @@
 context("Test ingesting a directory of uniform files")
 
-test_dir <- system.file("example_data", package = "ingestr")
-df <- ingestr::ingest_directory(test_dir, pattern = ".csv")
+test_direc <- system.file("example_data/campbell_directory", package = "ingestr")
+df <- ingest_directory(test_direc, ingest.function = ingest_campbell, pattern = ".dat")
+test_header <- ingest_header(test_direc) 
+test_length <- length(list.files(test_direc))
 
 test_that("output is a dataframe", {
   expect_s3_class(df, "data.frame")
+})
+
+# test that there are equal number of lines in header as the files that you read in
+test_that("number of lines equals number of files read",{
+  expect_equal(length(test_header)-1, test_length)
 })
 
